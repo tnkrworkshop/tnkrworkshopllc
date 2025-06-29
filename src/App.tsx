b57578f2a2719735b3ec653c186594a52dfdf03a
tnkrworkshop <tnkrworkshopllc@gmail.com>
2025-06-29 10:46:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/tnkrworkshop' : ''}>
+      <BrowserRouter basename={process.env.NODE_ENV === 'production' ? '/tnkrworkshopllc' : ''}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
