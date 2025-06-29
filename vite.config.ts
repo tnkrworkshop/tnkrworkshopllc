c8af4e36136b883e129868074c80ee31145fd8cc
tnkrworkshop <tnkrworkshopllc@gmail.com>
2025-06-29 10:40:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/tnkr-workshop/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/tnkrworkshop/' : '/',
   server: {
     host: "::",
     port: 8080,
